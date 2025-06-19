71aa045549a933d8d0d7ab1e1b7f60d7a9f22afc
Lee Leahy <lpleahyjr@gmail.com>
2025-06-19 09:57:29 -1000

    VK16K33: Add column map field to R4A_VK16K33 structure

--- src/R4A_I2C.h
+++ src/R4A_I2C.h
@@ -875,6 +875,7 @@ typedef struct _R4A_VK16K33
 {
     R4A_I2C_BUS * i2cBus;
     uint8_t i2cAddress;
+    const uint8_t * const columnMap;
     uint8_t columns;
     uint8_t rows;
     uint8_t brightness;
